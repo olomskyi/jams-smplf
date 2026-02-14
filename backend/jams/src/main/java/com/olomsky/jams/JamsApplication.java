f72e96b5dd29ee2fa9b9f3291c8ca2b95280f867
Oleksander Lomskyi <ylomsky@gmail.com>
2026-02-14 13:58:01 +0200

    Reworking properties usage.

--- backend/jams/src/main/java/com/olomsky/jams/JamsApplication.java
+++ backend/jams/src/main/java/com/olomsky/jams/JamsApplication.java
@@ -2,20 +2,13 @@ package com.olomsky.jams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class JamsApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-            .directory("backend/jams")
-            .load();
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-
 		SpringApplication.run(JamsApplication.class, args);
 	}
 }
